5cf635767f0997ee8ad000a36be0569a8d9f4f9c
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2014-07-22 00:42:31 +0530

    beaglelogic: Reorganize beaglelogic.h
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

--- hardware/beaglelogic/beaglelogic.h
+++ hardware/beaglelogic/beaglelogic.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the libsigrok and the BeagleLogic project.
+ * This file is part of the libsigrok project.
  *
  * Copyright (C) 2014 Kumar Abhishek <abhishek@theembeddedkitchen.net>
  *
@@ -34,7 +34,10 @@
 /* BeagleLogic device node name */
 #define BEAGLELOGIC_DEV_NODE        "/dev/beaglelogic"
 #define BEAGLELOGIC_SYSFS_ATTR(a)   "/sys/devices/virtual/misc/beaglelogic/"\
-                                      __STRING(a)
+					__STRING(a)
+
+/* Reproduced verbatim from beaglelogic.h in the kernel tree until the kernel
+ * module hits the mainline. Contains the ABI, so DO NOT TOUCH this section */
 
 /* ioctl calls that can be issued on /dev/beaglelogic */
 #define IOCTL_BL_GET_VERSION        _IOR('k', 0x20, uint32_t)
@@ -82,285 +85,129 @@ enum beaglelogic_sampleunit {
 	BL_SAMPLEUNIT_16_BITS = 0,
 	BL_SAMPLEUNIT_8_BITS
 };
+/* END beaglelogic.h */
 
-SR_PRIV int beaglelogic_open(void);
-SR_PRIV int beaglelogic_open_nonblock(void);
-SR_PRIV int beaglelogic_close(int fd);
-
-/* Read from the BeagleLogic file */
-SR_PRIV int beaglelogic_read(int fd, void *buf, size_t bytes);
-
-/* Gets and sets the kernel capture buffer size in bytes
- *
+/* For all the functions below:
  * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- * 	* size : pointer to var (for get) and value (for set)
+ * 	devc : Device context structure to operate on
  * Returns:
- * 	0 on success, -1 on failure
+ * 	SR_OK or SR_ERR
  */
-SR_PRIV int beaglelogic_get_buffersize(int fd, uint32_t *bufsize);
-SR_PRIV int beaglelogic_set_buffersize(int fd, uint32_t bufsize);
 
-/* Gets and sets the sample rate (in Hz)
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- * 	* samplerate : pointer to var (for get) and value (for set)
- * Returns:
- * 	0 on success, -1 on failure
- */
-SR_PRIV int beaglelogic_get_samplerate(int fd, uint32_t *samplerate);
-SR_PRIV int beaglelogic_set_samplerate(int fd, uint32_t samplerate);
+SR_PRIV int beaglelogic_open_nonblock(struct dev_context *devc);
+SR_PRIV int beaglelogic_close(struct dev_context *devc);
 
-/* Gets and sets the sample unit
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- * 	* sampleunit : pointer to var (for get) and value (for set)
- * Possible values:
- * 	* BL_SAMPLEUNIT_16_BITS : 16-bit samples
- * 	* BL_SAMPLEUNIT_8_BITS : 8-bit samples
- * Returns:
- * 	0 on success, -1 on failure
- */
-SR_PRIV int beaglelogic_get_sampleunit(int fd,
-		enum beaglelogic_sampleunit *sampleunit);
+SR_PRIV int beaglelogic_get_buffersize(struct dev_context *devc);
+SR_PRIV int beaglelogic_set_buffersize(struct dev_context *devc);
 
-SR_PRIV int beaglelogic_set_sampleunit(int fd,
-		enum beaglelogic_sampleunit sampleunit);
+SR_PRIV int beaglelogic_get_samplerate(struct dev_context *devc);
+SR_PRIV int beaglelogic_set_samplerate(struct dev_context *devc);
 
-/* Gets and sets the trigger flags
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- * 	* triggerflags : pointer to var (for get) and value (for set)
- * Possible values:
- * 	* BL_TRIGGERFLAGS_ONESHOT : One-shot
- * 	* BL_TRIGGERFLAGS_CONTINUOUS : Continuous
- * Returns:
- * 	0 on success, -1 on failure
- */
-SR_PRIV int beaglelogic_get_triggerflags(int fd,
-		enum beaglelogic_triggerflags *triggerflags);
+SR_PRIV int beaglelogic_get_sampleunit(struct dev_context *devc);
+SR_PRIV int beaglelogic_set_sampleunit(struct dev_context *devc);
 
-SR_PRIV int beaglelogic_set_triggerflags(int fd,
-		enum beaglelogic_triggerflags triggerflags);
+SR_PRIV int beaglelogic_get_triggerflags(struct dev_context *devc);
+SR_PRIV int beaglelogic_set_triggerflags(struct dev_context *devc);
 
-/* Polls for last error and returns the error code
- *
- * This function waits till the capture session ends, so may not be
- * called from the same thread that is reading data from the device
- *
- * Returns:
- * 	0 or 0x1nnnn, where nnnn: The buffer on which buffer overrun occurred
- */
-SR_PRIV int beaglelogic_getlasterror(void);
+/* Start and stop the capture operation */
+SR_PRIV int beaglelogic_start(struct dev_context *devc);
+SR_PRIV int beaglelogic_stop(struct dev_context *devc);
 
-/* Starts a logic capture
- *
- * This function may be called only while operating with mmap or testing
- * as the logic analyzer is triggered automatically on the first read() call
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *
- * Returns:
- * 	0 on success, -1 on failure
- */
-SR_PRIV inline int beaglelogic_start(int fd);
+/* Get the last error size */
+SR_PRIV int beaglelogic_getlasterror(struct dev_context *devc);
 
-/* Ends the active capture session
- * Use beaglelogic_getlasterror to busy-wait till the handle is released
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *
- * Returns:
- * 	0 on success, non-zero indicates some error
- */
-SR_PRIV inline int beaglelogic_stop(int fd) ;
+/* Gets the unit size of the capture buffer (usually 4 or 8 MB) */
+SR_PRIV int beaglelogic_get_bufunitsize(struct dev_context *devc);
 
-/* Invalidates the kernel buffer cache
- * To be used with mmap operations only.
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *
- * Returns:
- * 	0 on success, -1 on failure
- */
-SR_PRIV inline int beaglelogic_memcacheinvalidate(int fd);
-
-/* Gets the unit size of the capture buffer
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *
- * Returns:
- * 	the unit size
- */
-SR_PRIV int beaglelogic_get_bufunitsize(int fd);
-
-/* Maps the kernel buffer memory for user inspection / use
- * This gets the size from the beaglelogic device node
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *
- * Returns:
- * 	NULL if mapping failed, otherwise an address to the buffer
- */
-SR_PRIV void * beaglelogic_mmap(int fd);
-
-/* Destroys the memory mapping
- * This gets the size from the beaglelogic device node
- *
- * Parameters:
- * 	* fd : The file number to an open /dev/beaglelogic node
- *	* addr : The beginning of the mapping
- *
- * NOTE: Please destroy the previous mapping before setting the buffer
- * size with beaglelogic_set_buffersize. Otherwise strange things will
- * happen!
- *
- * Returns:
- * 	0 if successful
- */
-SR_PRIV int beaglelogic_munmap(int fd, void *addr);
-
-/* Busy-waits for the next sample buffer to be filled in
- * To be used in conjunction with mmap
- *
- * Parameters:
- * 	none. This uses sysfs attributes, hence no params are required
- *
- * Returns:
- * 	The index of the last buffer read. A negative value indicates the
- * 	state of BeagleLogic when not running (from enum beaglelogic_states)
- */
-SR_PRIV int beaglelogic_waitfornextbuffer(void);
+SR_PRIV int beaglelogic_mmap(struct dev_context *devc);
+SR_PRIV int beaglelogic_munmap(struct dev_context *devc);
 
 /* Sources */
-SR_PRIV inline int beaglelogic_open(void) {
-	return open(BEAGLELOGIC_DEV_NODE, O_RDONLY);
+SR_PRIV inline int beaglelogic_open_nonblock(struct dev_context *devc) {
+	devc->fd = open(BEAGLELOGIC_DEV_NODE, O_RDONLY | O_NONBLOCK);
+	return (devc->fd == -1 ? SR_ERR : SR_OK);
 }
 
-SR_PRIV inline int beaglelogic_open_nonblock(void) {
-	return open(BEAGLELOGIC_DEV_NODE, O_RDONLY);
+SR_PRIV inline int beaglelogic_close(struct dev_context *devc) {
+	return close(devc->fd);
 }
 
-SR_PRIV inline int beaglelogic_close(int fd) {
-	return close(fd);
+SR_PRIV inline int beaglelogic_get_buffersize(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_GET_BUFFER_SIZE, &devc->buffersize);
 }
 
-SR_PRIV inline int beaglelogic_read(int fd, void *buf, size_t bytes) {
-	return read(fd, buf, bytes);
+SR_PRIV inline int beaglelogic_set_buffersize(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_SET_BUFFER_SIZE, devc->buffersize);
 }
 
-SR_PRIV inline int beaglelogic_get_buffersize(int fd, uint32_t *bufsize) {
-	return ioctl(fd, IOCTL_BL_GET_BUFFER_SIZE, bufsize);
+/* This is treated differently as it gets a uint64_t while a uint32_t is read */
+SR_PRIV inline int beaglelogic_get_samplerate(struct dev_context *devc) {
+	uint32_t arg, err;
+	err = ioctl(devc->fd, IOCTL_BL_GET_SAMPLE_RATE, &arg);
+	devc->cur_samplerate = arg;
+	return err;
 }
 
-SR_PRIV inline int beaglelogic_set_buffersize(int fd, uint32_t bufsize) {
-	return ioctl(fd, IOCTL_BL_SET_BUFFER_SIZE, bufsize);
+SR_PRIV inline int beaglelogic_set_samplerate(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_SET_SAMPLE_RATE,
+			(uint32_t)devc->cur_samplerate);
 }
 
-SR_PRIV inline int beaglelogic_get_samplerate(int fd, uint32_t *samplerate) {
-	return ioctl(fd, IOCTL_BL_GET_SAMPLE_RATE, samplerate);
+SR_PRIV inline int beaglelogic_get_sampleunit(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_GET_SAMPLE_UNIT, &devc->sampleunit);
 }
 
-SR_PRIV inline int beaglelogic_set_samplerate(int fd, uint32_t samplerate) {
-	return ioctl(fd, IOCTL_BL_SET_SAMPLE_RATE, samplerate);
+SR_PRIV inline int beaglelogic_set_sampleunit(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_SET_SAMPLE_UNIT, devc->sampleunit);
 }
 
-SR_PRIV inline int beaglelogic_get_sampleunit(int fd,
-		enum beaglelogic_sampleunit *sampleunit) {
-	return ioctl(fd, IOCTL_BL_GET_SAMPLE_UNIT, sampleunit);
+SR_PRIV inline int beaglelogic_get_triggerflags(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_GET_TRIGGER_FLAGS, &devc->triggerflags);
 }
 
-SR_PRIV inline int beaglelogic_set_sampleunit(int fd,
-		enum beaglelogic_sampleunit sampleunit) {
-	return ioctl(fd, IOCTL_BL_SET_SAMPLE_UNIT, sampleunit);
+SR_PRIV inline int beaglelogic_set_triggerflags(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_SET_TRIGGER_FLAGS, devc->triggerflags);
 }
 
-SR_PRIV inline int beaglelogic_get_triggerflags(int fd,
-		enum beaglelogic_triggerflags *triggerflags) {
-	return ioctl(fd, IOCTL_BL_GET_TRIGGER_FLAGS, triggerflags);
-}
-
-SR_PRIV inline int beaglelogic_set_triggerflags(int fd,
-		enum beaglelogic_triggerflags triggerflags) {
-	return ioctl(fd, IOCTL_BL_SET_TRIGGER_FLAGS, triggerflags);
-}
-
-SR_PRIV int beaglelogic_getlasterror(void) {
-	int fd = open(BEAGLELOGIC_SYSFS_ATTR(lasterror), O_RDONLY);
+SR_PRIV int beaglelogic_getlasterror(struct dev_context *devc) {
+	int fd;
 	char buf[16];
-	char *endptr;
-	int lasterror, ret;
+	int ret;
 
-	if (!fd)
-		return -1;
+	if ((fd = open(BEAGLELOGIC_SYSFS_ATTR(lasterror), O_RDONLY)) == -1)
+		return SR_ERR;
 
 	if ((ret = read(fd, buf, 16)) < 0)
-		return -1;
+		return SR_ERR;
 
 	close(fd);
-	lasterror = strtoul(buf, &endptr, 10);
+	devc->last_error = strtoul(buf, NULL, 10);
 
-	return lasterror;
+	return SR_OK;
 }
 
-SR_PRIV inline int beaglelogic_start(int fd) {
-	return ioctl(fd, IOCTL_BL_START);
+SR_PRIV inline int beaglelogic_start(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_START);
 }
 
-SR_PRIV inline int beaglelogic_stop(int fd) {
-	return ioctl(fd, IOCTL_BL_STOP);
+SR_PRIV inline int beaglelogic_stop(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_STOP);
 }
 
-SR_PRIV inline int beaglelogic_memcacheinvalidate(int fd) {
-	return ioctl(fd, IOCTL_BL_CACHE_INVALIDATE);
+SR_PRIV int beaglelogic_get_bufunitsize(struct dev_context *devc) {
+	return ioctl(devc->fd, IOCTL_BL_GET_BUFUNIT_SIZE, &devc->bufunitsize);
 }
 
-SR_PRIV int beaglelogic_get_bufunitsize(int fd) {
-	size_t sz;
-	ioctl(fd, IOCTL_BL_GET_BUFUNIT_SIZE, &sz);
-	return sz;
+SR_PRIV int beaglelogic_mmap(struct dev_context *devc) {
+	if (!devc->buffersize)
+		beaglelogic_get_buffersize(devc);
+	devc->sample_buf = mmap(NULL, devc->buffersize,
+			PROT_READ, MAP_SHARED, devc->fd, 0);
+	return (devc->sample_buf == MAP_FAILED ? -1 : SR_OK);
 }
 
-void * beaglelogic_mmap(int fd) {
-	size_t sz;
-	void *addr;
-	if (beaglelogic_get_buffersize(fd, &sz))
-		return MAP_FAILED;
-	addr = mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
-	return addr;
-}
-
-SR_PRIV int beaglelogic_munmap(int fd, void *addr) {
-	size_t sz;
-	if (beaglelogic_get_buffersize(fd, &sz))
-		return -1;
-	return munmap(addr, sz);
-}
-
-SR_PRIV int beaglelogic_waitfornextbuffer(void) {
-	int fd = open(BEAGLELOGIC_SYSFS_ATTR(state), O_RDONLY);
-	char buf[16];
-	char *endptr;
-	int nextbuf, ret;
-
-	if (!fd)
-		return -1;
-
-	if ((ret = read(fd, buf, 16)) < 0)
-		return -1;
-
-	close(fd);
-	nextbuf = strtoul(buf, &endptr, 10);
-
-	return nextbuf;
+SR_PRIV int beaglelogic_munmap(struct dev_context *devc) {
+	return munmap(devc->sample_buf, devc->buffersize);
 }
 
 #endif /* BEAGLELOGIC_H_ */
